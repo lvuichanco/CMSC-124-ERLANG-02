f0b0856d68a3665c6891df6d70c6400fd534ab64
lvuichanco <80251741+lvuichanco@users.noreply.github.com>
2022-10-16 18:04:02 +0800

    Fix initialization

--- chat.erl
+++ chat.erl
@@ -5,17 +5,14 @@
 % When incoming connection is found, run chat function
 init_chat() ->
 	MyUsername = io:get_line("Enter Your Name: "),
-	receive
-		Chat_Node ->
-			register(chatProcess,spawn(chat,chatProcess,[MyUsername,Chat_Node]))
-	end.
+	register(chatProcess,spawn(chat,chatProcess,[MyUsername])).
 
 % Set username and connect to existing host
 % When connection is established, run chat function
 init_chat2(Chat_Node) ->
 	MyUsername = io:get_line("Enter Your Name: "),
-	spawn(chat,chatProcess,[MyUsername,Chat_Node]),
-	{init_chat,Chat_Node} ! node().
+	{chatProcess,Chat_Node} ! node(),
+	chatProcess(MyUsername,Chat_Node).
 
 % Accept input for message to send
 % While waiting for input, print any messages received
@@ -30,6 +27,27 @@ chatProcess(MyUsername,Chat_Node) ->
 		{_,bye} ->
 			io:format("Your partner disconnected~n");
 		{Username,Message} ->
-			io:format("~s: ~s", Username, Message),
+			io:format("~s: ~s", Username, Message)
+	end,
+	chatProcess(MyUsername,Chat_Node).
+
+% First chat process made by init_chat
+% After first exchange, switch to other chat process
+chatProcess(MyUsername) ->
+
+	receive
+		Chat_Node ->
+			% Send message to partner
+			Send = io:get_line("You: "),
+			{chatProcess,Chat_Node} ! {MyUsername,Send},
+
+			% Receive message from partner
+			receive
+				{_,bye} ->
+					io:format("Your partner disconnected~n");
+				{Username,Message} ->
+					io:format("~s: ~s", Username, Message)
+			end,
 			chatProcess(MyUsername,Chat_Node)
 	end.
+
